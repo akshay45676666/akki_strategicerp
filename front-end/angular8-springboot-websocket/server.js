858dc6131d2ac127d11cc8ac561486e1df92c7e0
vinitb <vinit@strategicerp.com>
2021-09-29 12:34:33 +0530

    updates to deploy to heroku

--- front-end/angular8-springboot-websocket/server.js
+++ front-end/angular8-springboot-websocket/server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/angular8-springboot-websocket'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular8-springboot-websocket/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8082);
